14a8337aac802172c4463944db828d1839d93130
banana <987906970@qq.com>
2019-09-09 17:48:47 +0800

    say hello class

---
+++ SayHello.java
@@ -0,0 +1,14 @@
+public class SayHello{
+	public static void main(String[] args){
+		System.out.println("Below is the function of sayhello");
+		SayHello sayhello=new SayHello();
+		sayhello(0);
+	}
+	public static void sayHello(int sum){
+		System.out.println("say Hello!"+sum);
+	}
+	
+	public static int sum(){
+		return 1+3;
+	}
+}
